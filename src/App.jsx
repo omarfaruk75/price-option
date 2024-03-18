a7cb0d32c25ee96a9b28c8d9e91aa05defc3cdf0
omar faruk <farukomarcpa@gmail.com>
2024-03-18 23:11:57 +0600

    new project start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='bg-slate-600 text-5xl'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
